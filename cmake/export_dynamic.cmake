ccffddcbfc94305a071b491ccdb24b83c333dae5
Timothy Brown <tbrown@freeshell.org>
2017-03-22 07:56:59 -0600

    Removing debugging info from Travis.
    Removing the debugging info from travis, it now finds the compiler.
    Trying to debug OS X as cmake is still adding the export dynamic flag.

--- cmake/export_dynamic.cmake
+++ cmake/export_dynamic.cmake
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------------------------
 # Darwin uses Mach-O binaries, so we don't need to export our own functions.
-if (NOT (${CMAKE_SYSTEM_NAME} STREQUAL "Darwin"))
+if (NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin"))
     set(LINK_EXPORT_DYNAMIC "-Wl,--export-dynamic")
     set(CMAKE_EXE_LINKER_FLAGS
         "${CMAKE_EXE_LINKER_FLAGS} ${LINK_EXPORT_DYNAMIC}")
