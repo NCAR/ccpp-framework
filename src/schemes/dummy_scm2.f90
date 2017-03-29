a666200f4464316258c5506d0cfd30a4614c9c94
Grant.Firl <Grant.Firl@noaa.gov>
2017-03-29 15:55:23 -0600

    added a second suite for testing purposes

--- src/schemes/dummy_scm2.f90
+++ src/schemes/dummy_scm2.f90
@@ -1,4 +1,4 @@
-module dummy_scm
+module dummy_scm2
     use, intrinsic :: iso_c_binding
 
     use            :: kinds,                                            &
@@ -10,11 +10,11 @@ module dummy_scm
     implicit none
 
     private
-    public :: dummy_scm_cap
+    public :: dummy_scm2_cap
 
     contains
 
-    subroutine dummy_scm_cap(ptr) bind(c)
+    subroutine dummy_scm2_cap(ptr) bind(c)
         implicit none
         type(c_ptr), intent(inout) :: ptr
 
@@ -28,16 +28,16 @@ module dummy_scm
         call phy_field_data(ap_data, 'northward_wind', v)
         call phy_field_data(ap_data, 'water_vapor_specific_humidity', q_v)
 
-        call dummy_scm_run(t, u, v, q_v)
-    end subroutine dummy_scm_cap
+        call dummy_scm2_run(t, u, v, q_v)
+    end subroutine dummy_scm2_cap
 
-    subroutine dummy_scm_run(t, u, v, q_v)
+    subroutine dummy_scm2_run(t, u, v, q_v)
         implicit none
         real(kind=r_dp), pointer, intent(inout) :: t(:), u(:), v(:), q_v(:)
 
-        print *, 'In DUMMY_SCM'
+        print *, 'In DUMMY_SCM2'
 
 
-    end subroutine dummy_scm_run
+    end subroutine dummy_scm2_run
 
-end module dummy_scm
+end module dummy_scm2
