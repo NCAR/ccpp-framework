720caf87134ad733a0951a2edad369e5931243ea
Timothy Brown <tbrown@freeshell.org>
2017-04-07 08:28:58 -0600

    Removing the kinds module.
    Removing all dependencies on the kinds module.

--- src/schemes/check/nan.f90
+++ src/schemes/check/nan.f90
@@ -6,8 +6,6 @@ module check_nans
 
     use, intrinsic :: iso_c_binding,                                   &
                       only: c_f_pointer, c_ptr
-    use            :: kinds,                                           &
-                      only: i_sp, r_dp
     use            :: ccpp_types,                                      &
                       only: ccpp_t
     use            :: ccpp_fields,                                     &
@@ -24,15 +22,12 @@ module check_nans
         type(c_ptr), intent(inout) :: ptr
 
         type(ccpp_t),     pointer  :: cdata
-        real(kind=r_dp),  pointer  :: v(:,:,:)
+        real,             pointer  :: v(:,:,:)
         integer                    :: i
         integer                    :: ierr
 
         call c_f_pointer(ptr, cdata)
 
-        call ccpp_fields_get(cdata, 'gravity', gravity, ierr)
-        call ccpp_fields_get(cdata, 'surface_temperature', surf_t, ierr)
-        call ccpp_fields_get(cdata, 'eastward_wind', u, ierr)
         call ccpp_fields_get(cdata, 'northward_wind', v, ierr)
 
         call test_run(gravity, u, v, surf_t)
@@ -41,18 +36,12 @@ module check_nans
 
     subroutine run(gravity, u, v, surf_t)
         implicit none
-        real(kind=r_dp), pointer, intent(inout) :: gravity
-        real(kind=r_dp), pointer, intent(inout) :: surf_t(:)
-        real(kind=r_dp), pointer, intent(inout) :: u(:,:,:)
-        real(kind=r_dp), pointer, intent(inout) :: v(:,:,:)
+        real, pointer, intent(inout) :: gravity
+        real, pointer, intent(inout) :: surf_t(:)
+        real, pointer, intent(inout) :: u(:,:,:)
+        real, pointer, intent(inout) :: v(:,:,:)
 
-        print *, 'In physics test_run'
-        print *, 'gravity: ', gravity
-        print *, 'surf_t:  ', surf_t
-        print *, 'updating u to be 10m/s'
-        u = 10.0_r_dp
-        print *, 'updating v to be -10m/s'
-        v = -10.0_r_dp
+        print *, 'In physics check nans run'
 
     end subroutine test_run
 
