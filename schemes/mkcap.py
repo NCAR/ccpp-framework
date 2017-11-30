aac733e4d0ba9707f053031c6eacb5e85155d098
Dom Heinzeller <dom.heinzeller@noaa.gov>
2017-11-30 11:12:38 -0700

    Rename mkcap to mkcap.py to indicate that this is a Python script. Also reflect changes in the attribute names for variables: "name" -> "standard_name", "id" -> "local_name"

--- schemes/mkcap.py
+++ schemes/mkcap.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 import os
 import sys
 import getopt
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
 
 #################### Main program routine
 def main():
@@ -57,7 +57,7 @@ def parse_scheme(filename):
 
     data = {}
 
-    tree = et.parse(filename)
+    tree = ET.parse(filename)
     root = tree.getroot()
 
     data['module'] = root.attrib.get('module')
@@ -70,11 +70,12 @@ def parse_scheme(filename):
 
         for var in sub.findall('var'):
             v = Var()
-            v.name  = var.find('name').text
-            v.units = var.find('units').text
-            v.id    = var.find('id').text
-            v.type  = var.find('type').text
-            v.rank  = int(var.find('rank').text)
+            v.standard_name = var.find('standard_name').text
+            #v.long_name     = var.find('long_name').text
+            v.units         = var.find('units').text
+            v.local_name    = var.find('local_name').text
+            v.type          = var.find('type').text
+            v.rank          = int(var.find('rank').text)
             data['subs'][name]['vars'].append(v)
 
     return data
@@ -98,22 +99,33 @@ def eprint(*args, **kwargs):
 class Var(object):
 
     def __init__(self, **kwargs):
-        self._name  = None
-        self._units = None
-        self._id    = None
-        self._type  = None
-        self._rank  = None
+        self._standard_name = None
+        self._long_name     = None
+        self._units         = None
+        self._local_name    = None
+        self._type          = None
+        self._rank          = None
+        self._container     = None
         for key, value in kwargs.items():
             setattr(self, "_"+key, value)
 
     @property
-    def name(self):
+    def standard_name(self):
         '''Get the name of the variable.'''
-        return self._name
+        return self._standard_name
 
-    @name.setter
-    def name(self, value):
-        self._name = value
+    @standard_name.setter
+    def standard_name(self, value):
+        self._standard_name = value
+
+    @property
+    def long_name(self):
+        '''Get the name of the variable.'''
+        return self._long_name
+
+    @long_name.setter
+    def long_name(self, value):
+        self._long_name = value
 
     @property
     def units(self):
@@ -125,13 +137,13 @@ class Var(object):
         self._units = value
 
     @property
-    def id(self):
-        '''Get the identification of the variable.'''
-        return self._id
+    def local_name(self):
+        '''Get the local variable name of the variable.'''
+        return self._local_name
 
-    @id.setter
-    def id(self, value):
-        self._id = value
+    @local_name.setter
+    def local_name(self, value):
+        self._local_name = value
 
     @property
     def type(self):
@@ -149,26 +161,128 @@ class Var(object):
 
     @rank.setter
     def rank(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Invalid type for variable property rank, must be integer')
         if (value == 0):
             self._rank = ''
         else:
             self._rank = '('+ ','.join([':'] * value) +')'
 
+    @property
+    def intent(self):
+        '''Get the intent of the variable.'''
+        return self._intent
+
+    @intent.setter
+    def intent(self, value):
+        if not value in ['none', 'in', 'out', 'inout']:
+            raise ValueError('Invalid value {0} for variable property intent'.format(value))
+        self._intent = value
+
+    @property
+    def optional(self):
+        '''Get the optional of the variable.'''
+        return self._optional
+
+    @optional.setter
+    def optional(self, value):
+        if not value in ['T', 'F']:
+            raise ValueError('Invalid value {0} for variable property optional'.format(value))
+        self._optional = value
+
+    @property
+    def container(self):
+        '''Get the container of the variable.'''
+        return self._container
+
+    @container.setter
+    def container(self, value):
+        self._container = value
+
+    def compatible(self, other):
+        return self.standard_name == other.standard_name \
+            and self.long_name == other.long_name \
+            and self.units == other.units \
+            and self.type == other.type \
+            and self.rank == other.rank
+
     def print_def(self):
         '''Print the definition line for the variable.'''
-        str = "{s.type}, pointer     :: {s.id}{s.rank}"
+        str = "{s.type}, pointer     :: {s.local_name}{s.rank}"
         return str.format(s=self)
 
     def print_get(self):
         '''Print the data retrieval line for the variable.'''
 
         str='''
-        call ccpp_fields_get(cdata, '{s.name}', {s.id}, ierr)
+        call ccpp_fields_get(cdata, '{s.standard_name}', {s.local_name}, ierr)
         if (ierr /= 0) then
-            call ccpp_error('Unable to retrieve {s.name}')
+            call ccpp_error('Unable to retrieve {s.standard_name}')
             return
         end if'''
         return str.format(s=self)
+
+    def print_debug(self):
+        '''Print the data retrieval line for the variable.'''
+    
+        str='''Contents of {s} (* = mandatory for compatibility):
+        standard_name = {s.standard_name} *
+        long_name     = {s.long_name} *
+        units         = {s.units} *
+        local_name    = {s.local_name}
+        type          = {s.type} *
+        rank          = {s.rank} *
+        intent        = {s.intent}
+        optional      = {s.optional}
+        container     = {s.container}'''
+        return str.format(s=self)
+    
+    @classmethod
+    def from_table(cls, columns, data):
+        # DH* - workaround to use the existing table headers
+        standard_name = data[columns.index('longname')]
+        #standard_name = data[columns.index('standard_name')]
+        long_name = data[columns.index('description')]
+        #long_name = data[columns.index('long_name')]
+        units = data[columns.index('units')]
+        local_name = data[columns.index('local var name')]
+        #local_name = data[columns.index('local_name')]
+        type = data[columns.index('type')]
+        rank = data[columns.index('rank')]
+        intent = data[columns.index('intent')]
+        optional = data[columns.index('optional')]
+        # *DH
+        return cls(standard_name = standard_name,
+                   long_name = long_name,
+                   units = units,
+                   local_name = local_name,
+                   type = type,
+                   rank = rank,
+                   intent = intent,
+                   optional = optional,
+                   )
+
+    def to_xml(self, element):
+        element.set('name', self._standard_name)
+        sub_element = ET.SubElement(element, 'standard_name')
+        sub_element.text = self._standard_name
+        sub_element = ET.SubElement(element, 'long_name')
+        sub_element.text = self._long_name
+        sub_element = ET.SubElement(element, 'units')
+        sub_element.text = self._units
+        sub_element = ET.SubElement(element, 'local_name')
+        sub_element.text = self._local_name
+        sub_element = ET.SubElement(element, 'type')
+        sub_element.text = self._type
+        sub_element = ET.SubElement(element, 'rank')
+        sub_element.text = self._rank
+        sub_element = ET.SubElement(element, 'intent')
+        sub_element.text = self._intent
+        sub_element = ET.SubElement(element, 'optional')
+        sub_element.text = self._optional
+        sub_element = ET.SubElement(element, 'container')
+        sub_element.text = self._container
+        return element
 
 ###############################################################################
 class Cap(object):
@@ -248,7 +362,7 @@ module {module}_cap
         for (k, v) in data['subs'].items():
             var_defs = "\n".join([" "*8 + x.print_def() for x in v['vars']])
             var_gets = "\n".join([x.print_get() for x in v['vars']])
-            args = ','.join(["{0}={0}".format(x.id) for x in v['vars']])
+            args = ','.join(["{0}={0}".format(x.local_name) for x in v['vars']])
             f.write(Cap.sub.format(subroutine=k,
                                    var_defs=var_defs,
                                    var_gets=var_gets,
