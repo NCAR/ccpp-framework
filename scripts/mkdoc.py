42f54cea1bc7ecf75fad91b6daf67b95a8d27b41
Laurie Carson <carson@ucar.edu>
2018-04-09 10:04:23 -0600

    Release v1.0 branch, merge to master (#53)
    Merge updates to the release branch back to master (primarily documentation updates, a few build updates)

--- scripts/mkdoc.py
+++ scripts/mkdoc.py
@@ -5,7 +5,7 @@
 
 import logging
 
-from common import decode_container
+from common import decode_container, escape_tex
 
 #################### Main program routine
 def main():
@@ -25,6 +25,7 @@ def metadata_to_html(metadata, model, filename):
     shading = { 0 : 'darkgray', 1 : 'lightgray' }
     success = True
 
+    # Header
     html = '''<html>
 <title>CCPP variables provided by model {model}</title>
 <body>
@@ -59,8 +60,111 @@ def metadata_to_html(metadata, model, filename):
   <td bgcolor="{bgcolor}" align="left" >{v.local_name}   </td>
 </tr>'''.format(v=var, rank=var.rank.count(':'), container = decode_container(var.container), bgcolor=shading[count])
             html += line
+
+    # Footer
+    html += '''</table>
+</body>
+</html>
+'''
+
     with open(filename, 'w') as f:
         f.write(html)
+
+    logging.info('Metadata table for model {0} written to {1}'.format(model, filename))
+    return success
+
+
+def metadata_to_latex(metadata_define, metadata_request, model, filename):
+    """Create a metadata table for each variable provided and/or requested"""
+
+    # Set debug to true if logging level is debug
+    #debug = logging.getLogger().getEffectiveLevel() == logging.DEBUG
+
+    shading = { 0 : 'darkgray', 1 : 'lightgray' }
+    success = True
+
+    var_names = sorted(list(set(metadata_define.keys() + metadata_request.keys())))
+
+    latex = '''\\documentclass[12pt,letterpaper,oneside]{{scrbook}}
+
+\\usepackage{{import}}
+\\import{{../common/}}{{gmtb.sty}}
+\\renewcommand{{\\thesection}}{{\\arabic{{section}}}}
+\\renewcommand{{\\thesubsection}}{{\\arabic{{section}}.\\arabic{{subsection}}}}
+
+\\begin{{document}}
+
+\\section{{CCPP variables provided by model {model} vs requested by pool of physics}}\\label{{sec_ccpp_variables}}
+\\subsection{{List of variables}}
+\\begin{{longtable}}{{l}}'''.format(model=model)
+
+    for var_name in var_names:
+        if var_name in metadata_define.keys():
+            var = metadata_define[var_name][0]
+        else:
+            var = metadata_request[var_name][0]
+        line = '''
+\hyperlink{{{standard_name_ref}}}{{\\blue\\underline{{\\execout{{{standard_name}}}}}}} \\\\'''.format(standard_name=escape_tex(var.standard_name), standard_name_ref=var.standard_name)
+        latex += line
+
+    latex += '''
+\\end{longtable}\\pagebreak
+\\subsection{Description of variables}
+{{\\small\\begin{description}
+'''
+
+    for var_name in var_names:
+        if var_name in metadata_define.keys():
+            var = metadata_define[var_name][0]
+            target = escape_tex(decode_container(var.container))
+            local_name = escape_tex(var.local_name)
+        else:
+            var = metadata_request[var_name][0]
+            target = 'MISSING'
+            local_name = 'MISSING'
+        if var_name in metadata_request.keys():
+            requested_list = [ escape_tex(decode_container(v.container)) for v in metadata_request[var_name] ]
+            # for the purpose of the table, just output the name of the subroutine
+            for i in xrange(len(requested_list)):
+                entry = requested_list[i]
+                requested_list[i] = entry[entry.find('SUBROUTINE')+len('SUBROUTINE')+1:]
+            requested = '\\newline '.join(sorted(requested_list))
+        else:
+            requested = 'NOT REQUESTED'
+
+        # Create output
+        text = '''
+\\begin{{samepage}}\\item{{
+\hypertarget{{{standard_name_ref}}}{{\\blue\\exec{{{standard_name}}}}}}}\\\\ \\nopagebreak
+\\begin{{tabular}}{{ll}}
+\\execout{{long\_name }} & \\execout{{{long_name}          }} \\\\
+\\execout{{units      }} & \\execout{{{units}              }} \\\\
+\\execout{{rank       }} & \\execout{{{rank}               }} \\\\
+\\execout{{type       }} & \\execout{{{type}               }} \\\\
+\\execout{{kind       }} & \\execout{{{kind}               }} \\\\
+\\execout{{source     }} & \\execout{{{target}             }} \\\\
+\\execout{{local\_name}} & \\execout{{{local_name}         }} \\\\
+\\execout{{requested  }} & \\execout{{\\vtop{{{requested}}}}} \\\\
+\\end{{tabular}}
+\\vspace{{4pt}}
+\\end{{samepage}}'''.format(standard_name=escape_tex(var.standard_name), standard_name_ref=var.standard_name,
+                          long_name=escape_tex(var.long_name),
+                          units=escape_tex(var.units),
+                          rank=var.rank.count(':'),
+                          type=escape_tex(var.type),
+                          kind=escape_tex(var.kind),
+                          target=target,
+                          local_name=local_name,
+                          requested=requested)
+        latex += text
+    # Footer
+    latex += '''
+\\end{description}}}
+\\end{document}
+'''
+
+    with open(filename, 'w') as f:
+        f.write(latex)
 
     logging.info('Metadata table for model {0} written to {1}'.format(model, filename))
     return success
