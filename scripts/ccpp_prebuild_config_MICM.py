d7a5e05f18213134587f81237a4d02f198490478
Cheryl Craig <cacraig@ucar.edu>
2018-06-18 16:38:09 -0600

    add scripts/ccpp_prebuild_config_MICM.py

---
+++ scripts/ccpp_prebuild_config_MICM.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+# CCPP prebuild config for MICM 
+
+
+###############################################################################
+# Definitions                                                                 #
+###############################################################################
+
+# Add all files with metadata tables on the host model side,
+# relative to basedir = top-level directory of host model
+VARIABLE_DEFINITION_FILES = [
+    'micm/src/micm_type_defs.f90',
+    ]
+
+# Can be empty, since all physics schemes and their
+# dependencies are hardcoded in CMakeLists in
+# ccpp-physics - to fix, c.f. FV3 v1
+SCHEME_FILES_DEPENDENCIES = []
+
+# Add all physics scheme files relative to basedir
+SCHEME_FILES = [
+    'ccpp-physics/physics/chemistry.F90'
+    ]
+
+# Auto-generated makefile/cmakefile snippets that contain all schemes
+SCHEMES_MAKEFILE = 'ccpp-physics/CCPP_SCHEMES.mk'
+SCHEMES_CMAKEFILE = 'ccpp-physics/CCPP_SCHEMES.cmake'
+
+# CCPP host cap in which to insert the ccpp_field_add statements;
+# determines the directory to place ccpp_{modules,fields}.inc
+TARGET_FILES = [
+    'micm/src/micm.f90',
+    ]
+
+# Auto-generated makefile/cmakefile snippets that contain all caps
+CAPS_MAKEFILE = 'ccpp-physics/CCPP_CAPS.mk'
+CAPS_CMAKEFILE = 'ccpp-physics/CCPP_CAPS.cmake'
+
+# Directory where to put all auto-generated physics caps
+CAPS_DIR = 'ccpp-physics/physics'
+
+# Optional arguments - only required for schemes that use
+# optional arguments. ccpp_prebuild.py will throw an exception
+# if it encounters a scheme subroutine with optional arguments
+# if no entry is made here. Possible values are: 'all', 'none',
+# or a list of standard_names: [ 'var1', 'var3' ].
+OPTIONAL_ARGUMENTS = {
+    'rrtmg_sw' : {
+        'rrtmg_sw_run' : [
+            'tendency_of_air_temperature_due_to_shortwave_heating_assuming_clear_sky_on_radiation_time_step',
+            'components_of_surface_downward_shortwave_fluxes',
+            'cloud_liquid_water_path',
+            'mean_effective_radius_for_liquid_cloud',
+            'cloud_ice_water_path',
+            'mean_effective_radius_for_ice_cloud',
+            'cloud_rain_water_path',
+            'mean_effective_radius_for_rain_drop',
+            'cloud_snow_water_path',
+            'mean_effective_radius_for_snow_flake',
+            ],
+        },
+    'rrtmg_lw' : {
+        'rrtmg_lw_run' : [
+            'tendency_of_air_temperature_due_to_longwave_heating_assuming_clear_sky_on_radiation_time_step',
+            'cloud_liquid_water_path',
+            'mean_effective_radius_for_liquid_cloud',
+            'cloud_ice_water_path',
+            'mean_effective_radius_for_ice_cloud',
+            'cloud_rain_water_path',
+            'mean_effective_radius_for_rain_drop',
+            'cloud_snow_water_path',
+            'mean_effective_radius_for_snow_flake',
+            ],
+        },
+    #'subroutine_name_1' : 'all',
+    #'subroutine_name_2' : 'none',
+    #'subroutine_name_2' : [ 'var1', 'var3'],
+    }
+
+# Names of Fortran include files in the host model cap (do not change);
+# both files will be written to the directory of each target file
+MODULE_INCLUDE_FILE = 'ccpp_modules.inc'
+FIELDS_INCLUDE_FILE = 'ccpp_fields.inc'
+
+# HTML document containing the model-defined CCPP variables
+HTML_VARTABLE_FILE = 'ccpp-physics/CCPP_VARIABLES_MICM.html'
+
+# LaTeX document containing the provided vs requested CCPP variables
+LATEX_VARTABLE_FILE = 'ccpp-framework/doc/DevelopersGuide/CCPP_VARIABLES_MICM.tex'
+
+
+###############################################################################
+# Template code to generate include files                                     #
+###############################################################################
+
+# Name of the CCPP data structure in the host model cap;
+# in the case of MICM, this is a vector with loop index i
+CCPP_DATA_STRUCTURE = 'cdata(i)'
+
+# Modules to load for auto-generated ccpp_field_add code
+# in the host model cap (e.g. error handling)
+MODULE_USE_TEMPLATE_HOST_CAP = \
+'''
+use ccpp_errors, only: ccpp_error
+'''
+
+# Modules to load for auto-generated ccpp_field_get code
+# in the physics scheme cap (e.g. derived data types)
+MODULE_USE_TEMPLATE_SCHEME_CAP = \
+'''
+       use machine, only: kind_phys
+       use GFS_typedefs, only: GFS_statein_type,  GFS_stateout_type,    &
+                               GFS_sfcprop_type,                        &
+                               GFS_coupling_type, GFS_control_type,     &
+                               GFS_grid_type,     GFS_tbd_type,         &
+                               GFS_cldprop_type,  GFS_radtend_type,     &
+                               GFS_diag_type,     GFS_interstitial_type,&
+                               GFS_init_type
+'''
