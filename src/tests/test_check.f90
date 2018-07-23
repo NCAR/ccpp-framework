02a075015a3fd78583aa8751919d483da8944991
Dom Heinzeller <climbfuji@ymail.com>
2018-07-23 14:36:05 -0600

    src/tests/test_check.f90: update test with metadata table and new CCPP API

--- src/tests/test_check.f90
+++ src/tests/test_check.f90
@@ -16,6 +16,15 @@
 !!
 program test_check
 
+!! \section arg_table_test_check
+!! | local_name   | standard_name              | long_name                  | units   | rank | type      |   kind   | intent | optional |
+!! |-------------------------------------------|----------------------------|---------|------|-----------|----------|--------|----------|
+!! | gravity      | gravitational_acceleration | gravitational acceleration | m s-2   |    0 | real      |          | none   | F        |
+!! | u            | x_wind                     | zonal wind                 | m s-1   |    2 | real      |          | none   | F        |
+!! | v            | y_wind                     | meridional wind            | m s-1   |    2 | real      |          | none   | F        |
+!! | tsfc         | surface_skin_temperature   | surface skin temperature   | K       |    1 | real      |          | none   | F        |
+!!
+
     use, intrinsic :: iso_c_binding,                                   &
                       only: c_loc, c_f_pointer
     use            :: ccpp_api,                                        &
@@ -23,20 +32,22 @@ program test_check
                             ccpp_t,                                    &
                             ccpp_init,                                 &
                             ccpp_finalize,                             &
+                            ccpp_physics_init,                         &
                             ccpp_physics_run,                          &
+                            ccpp_physics_finalize,                     &
                             ccpp_field_add
 
     implicit none
 
-    type(ccpp_t), target                         :: cdata
-    character(len=CCPP_STR_LEN)                  :: filename
-    integer                                      :: len
-    integer                                      :: ierr
-    integer                                      :: asize
-    real, target                                 :: gravity
-    real, target, allocatable, dimension(:)      :: surf_t
-    real, target, allocatable, dimension(:,:,:)  :: u
-    real, target, allocatable, dimension(:,:,:)  :: v
+    type(ccpp_t), target                      :: cdata
+    character(len=CCPP_STR_LEN)               :: filename
+    integer                                   :: len
+    integer                                   :: ierr
+    integer                                   :: asize
+    real, target                              :: gravity
+    real, target, allocatable, dimension(:,:) :: u
+    real, target, allocatable, dimension(:,:) :: v
+    real, target, allocatable, dimension(:)   :: tsfc
 
     ierr = 0
 
@@ -47,19 +58,19 @@ program test_check
 
     ! Allocate the data
     asize = 5
-    allocate(surf_t(asize), stat=ierr)
+    allocate(tsfc(asize), stat=ierr)
     if (ierr /= 0) then
             print *, 'Unable to allocate surface temperature array'
             call exit(1)
     end if
 
-    allocate(u(asize,asize,asize), stat=ierr)
+    allocate(u(asize,asize), stat=ierr)
     if (ierr /= 0) then
             print *, 'Unable to allocate U array'
             call exit(1)
     end if
 
-    allocate(v(asize,asize,asize), stat=ierr)
+    allocate(v(asize,asize), stat=ierr)
     if (ierr /= 0) then
             print *, 'Unable to allocate U array'
             call exit(1)
@@ -67,40 +78,67 @@ program test_check
 
     ! Generate data to pass into a physics driver
     gravity = 9.80665
-    surf_t = [290.0, 291.0, 292.0, 293.0, 294.0]
+    tsfc = [290.0, 291.0, 292.0, 293.0, 294.0]
     u = 0.0
     v = 10.0
 
-    ! Initalize the CCPP (with the filename of the suite to load).
+    ! Initalize the CCPP framework (with the filename of the suite to load)
     call ccpp_init(filename, cdata, ierr)
     if (ierr /= 0) then
         call exit(1)
     end if
 
     ! Add all the fields we want to expose to the physics driver.
-    call ccpp_field_add(cdata, 'gravity', gravity, ierr, 'm s-2')
+    call ccpp_field_add(cdata, 'gravitational_acceleration', gravity, ierr, 'm s-2')
     if (ierr /= 0) then
-            call exit(1)
+        call exit(1)
     end if
 
-    call ccpp_field_add(cdata, 'surface_temperature', surf_t, ierr, 'K')
+    call ccpp_field_add(cdata, 'surface_skin_temperature', tsfc, ierr, 'K')
+    if (ierr /= 0) then
+        call exit(1)
+    end if
 
-    call ccpp_field_add(cdata, 'eastward_wind', u, ierr, 'm s-1')
+    call ccpp_field_add(cdata, 'x_wind', u, ierr, 'm s-1')
+    if (ierr /= 0) then
+        call exit(1)
+    end if
 
-    call ccpp_field_add(cdata, 'northward_wind', v, ierr, 'm s-1')
+    call ccpp_field_add(cdata, 'y_wind', v, ierr, 'm s-1')
+    if (ierr /= 0) then
+        call exit(1)
+    end if
 
+    ! Initialize the test scheme
+    call ccpp_physics_init(cdata, ierr=ierr)
+    if (ierr /= 0) then
+        call exit(1)
+    end if
+
+    ! Run the test scheme
     call ccpp_physics_run(cdata, scheme_name="test", ierr=ierr)
+    if (ierr /= 0) then
+        print *, "Call to scheme test failed, error message: '" // trim(cdata%errmsg) // "'"
+        call exit(1)
+    end if
 
     print *, 'In test dummy main'
     print *, 'gravity: ', gravity
-    print *, 'surf_t:  ', surf_t(1:2)
-    print *, 'u: ', u(1,1,1)
-    print *, 'v: ', v(1,1,1)
+    print *, 'tsfc:  ', tsfc(1:2)
+    print *, 'u: ', u(1,1)
+    print *, 'v: ', v(1,1)
 
+    ! Finalize the test scheme
+    call ccpp_physics_finalize(cdata, ierr=ierr)
+    if (ierr /= 0) then
+        call exit(1)
+    end if
+
+    ! Finalize the CCPP framework
     call ccpp_finalize(cdata, ierr)
 
-    if (allocated(surf_t)) then
-        deallocate(surf_t)
+    if (allocated(tsfc)) then
+        deallocate(tsfc)
     end if
 
     if (allocated(u)) then
