ec0f146c83b7b6aa649ff0fb27795b847576d149
Steve Goldhaber <goldy@ucar.edu>
2018-11-05 17:33:40 -0700

    Improvement to ParseContext to include regions such as module or type

--- scripts/fortran_parser/parse_fortran_file.py
+++ scripts/fortran_parser/parse_fortran_file.py
@@ -2,4 +2,12 @@
 """
 Tool to parse a Fortran file and return signature information
 from metadata tables.
+At the file level, we allow only PROGRAM blocks and MODULE blocks.
+Subroutines, functions, or data are not supported outside a MODULE.
 """
+
+import re
+import logging
+from parse_tools import ParseContext
+
+def parse_program(lines, line_start):
--- scripts/parse_tools/parse_tools.py
+++ scripts/parse_tools/parse_tools.py
@@ -1,8 +1,74 @@
 #!/usr/bin/env python
 
 import logging
+import collections
+import copy
 
 ## Classes to aid the parsing process
+
+########################################################################
+
+class ParseSyntaxError(ValueError):
+    """Exception that is aware of parsing context"""
+    def __init__(self, token_type, token, context):
+        if context is None:
+            message = "Invalid {}, '{}'".format(token_type, token)
+        else:
+            message = "Invalid {}, '{}', at {}".format(token_type, token, context.ctx_str())
+        # End if
+        super(ParseSyntaxError, self).__init__(message)
+
+########################################################################
+
+class ParseInternalError(StandardError):
+    """Exception for internal parser use errors"""
+    def __init__(self, errmsg, context):
+        if context is None:
+            message = errmsg
+        else:
+            message = "{}, at {}".format(errmsg, context.ctx_str())
+        # End if
+        super(ParseInternalError, self).__init__(message)
+
+########################################################################
+
+class ParseContextError(ValueError):
+    """Exception for errors using ParseContext"""
+    def __init__(self, errmsg, context):
+        if context is None:
+            message = "{}".format(errmsg)
+        else:
+            message = "{}, at {}".format(errmsg, context.ctx_str())
+        # End if
+        super(ParseContextError, self).__init__(message)
+
+########################################################################
+
+class ContextRegion(collections.Iterable):
+    """Class to imitate the LIFO nature of program language blocks"""
+
+    def __init__(self):
+        self._lifo = list()
+
+    def push(self, rtype, rname):
+        """Push a new region onto the stack"""
+        self._lifo.append([rtype, rname])
+
+    def pop(self):
+        """Remove the top item from the stack"""
+        return self._lifo.pop()
+
+    def __iter__(self):
+        for item in self._lifo:
+            yield item[0]
+
+    def __len__(self):
+        return len(self._lifo)
+
+    def __getitem__(self, index):
+        return self._lifo[index]
+
+########################################################################
 
 class ParseContext(object):
     """A class for keeping track of a parsing position
@@ -11,9 +77,9 @@ class ParseContext(object):
     >>> ParseContext("source.F90", 32)
     Traceback (most recent call last):
     ValueError: ParseContext linenum must be an int
-    >>> ParseContext(32, 90)
+    >>> ParseContext(32, 90) #doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
-    ValueError: ParseContext filenum must be an int
+    ValueError: ParseContext filenum must be a string
     >>> ParseContext(32, "source.F90").ctx_str()
     'source.F90:32'
     >>> ParseContext().ctx_str()
@@ -30,15 +96,22 @@ class ParseContext(object):
             linenum = 1
         elif type(linenum) != int:
             raise ValueError('ParseContext linenum must be an int')
+        # End if
         if context is not None:
             # If context is passed, ignore filename
             filename = context.filename
         elif filename is None:
             filename = "<standard input>"
         elif type(filename) != str:
-            raise ValueError('ParseContext filenum must be an int')
+            raise ValueError('ParseContext filenum must be a string')
+        # End if
         self.linenum = linenum
         self.filename = filename
+        if context is not None:
+            self.regions = copy.deepcopy(context.regions)
+        else:
+            self.regions = ContextRegion()
+        # End if
 
     @property
     def line_num(self):
@@ -50,32 +123,53 @@ class ParseContext(object):
         self.linenum = newnum
 
     def ctx_str(self):
+        "Return a string representing the location in a file"
         return "{}:{}".format(self.filename, self.linenum)
 
     def increment(self, inc=1):
+        "Increment the location within a file"
         self.linenum = self.linenum + inc
 
-########################################################################
-
-class ParseSyntaxError(ValueError):
-    """Exception that is aware of parsing context"""
-    def __init__(self, token_type, token, context):
-        if context is None:
-            message = "Invalid {}, '{}'".format(token_type, token)
+    def enter_region(self, region_type, region_name=None, nested_ok=True):
+        """Mark the entry of a region (e.g., DDT, module, function).
+        If nested_ok == False, throw an exception if the context is already
+        inside a region with the same type."""
+        if (not nested_ok) and (region_type in self.regions):
+            raise ParseContextException("Cannot enter a nested {} region".format(region_type), self)
         else:
-            message = "Invalid {}, '{}', at {}".format(token_type, token, context.ctx_str())
-        # End if
-        super(ParseSyntaxError, self).__init__(message)
+            self.regions.push(region_type, region_name)
 
-class ParseInternalError(StandardError):
-    """Exception for internal parser use errors"""
-    def __init__(self, errmsg, context):
-        if context is None:
-            message = errmsg
+    def leave_region(self, region_type, region_name=None):
+        """Mark the exit from a region. Check region name if possible"""
+        if len(self.regions) == 0:
+            raise ParseContextException("Cannot exit, not currently in any region", self)
         else:
-            message = "{}, at {}".format(errmsg, context.ctx_str())
+            curr_type, curr_name = self.regions.pop()
+            if curr_type != region_type:
+                raise ParseContextException("Trying to exit {} region while currently in {} region".format(region_type, curr_type), self)
+            elif (region_name is not None) and (curr_name is not None):
+                if region_name != curr_name:
+                    raise ParseContextException("Trying to exit {} {} while currently in {} {}".format(region_type, region_name, curr_type, curr_name), self)
+                # End if
+            elif (region_name is not None) and (curr_name is None):
+                raise ParseContextException("Trying to exit {} {} while currently in unnamed {} region".format(region_type, region_name, curr_type), self)
+            # End if
         # End if
-        super(ParseInternalError, self).__init__(message)
+
+    def region_str(self):
+        """Create a string describing the current region"""
+        rgn_str = ""
+        for index in len(self.regions):
+            rtype, rname = self.regions[index]
+            if len(rgn_str) > 0:
+                rgn_str = rgn_str + " ==> "
+            # End if
+            rgn_str = rgh_str + "{}".format(rtype)
+            if rname is not None:
+                rgn_str = rgh_str + "{}".format(rname)
+            # End if
+        # End for
+        return rgn_str
 
 ########################################################################
 
