40cc429580b9d564eb78f441cdf021becc6e95f3
gold2718 <goldy@ucar.edu>
2018-11-27 16:42:11 -0700

    UI complete for capgen for now

--- scripts/ccpp_capgen.py
+++ scripts/ccpp_capgen.py
@@ -18,6 +18,16 @@ def check_for_existing_file(filename, description):
 ###############################################################################
     if not os.path.exists(filename):
         logger.error("{}, '{}', must exist".format(description, filename))
+    # End if (else just return)
+
+###############################################################################
+def check_for_writeable_file(filename, description):
+###############################################################################
+    if os.path.exists(filename) and not os.access(filename, os.W_OK):
+        logger.error("Cannot write {}, '{}'".format(description, filename))
+    elif not os.access(os.path.dirname(filename), os.W_OK):
+        logger.error("Cannot write {}, '{}'".format(description, filename))
+    # End if (else just return)
 
 ###############################################################################
 def parse_command_line(args, description):
@@ -31,8 +41,9 @@ def parse_command_line(args, description):
     parser.add_argument("scheme_pathnames", metavar='<scheme files filename>',
                         type=str, help="File with scheme filenames to process")
 
-    parser.add_argument("--sdf-pathlist", metavar='<sdf files filename>',
-                        type=str, help="File with names of suite definition fileames to process")
+    parser.add_argument("--sdf-pathlist", metavar='<sdf file(s)>',
+                        type=str,
+                        help="File with names of suite definition fileames to process or name of single XML SDF file")
 
     parser.add_argument("--host-model-def", metavar='<host model def filename>',
                         type=str,
@@ -44,13 +55,18 @@ def parse_command_line(args, description):
 
     parser.add_argument("--cap-pathlist", type=str,
                         metavar='<filename for list of cap filenames>',
-                        default=os.path.join(os.getcwd(), "capfiles.txt"),
+                        default="capfiles.txt",
                         help="Filename for list of generated cap files")
 
     parser.add_argument("--host-pathlist", type=str,
                         metavar='<filename for generated host filenames>',
-                        default=os.path.join(os.getcwd(), "hostfiles.txt"),
+                        default="hostfiles.txt",
                         help="Filename for list of generated host files")
+
+    parser.add_argument("--output-root", type=str,
+                        metavar='<directory for generated files>',
+                        default=os.getcwd(),
+                        help="directory for generated files")
 
     parser.add_argument("--generate-docfiles",
                         metavar='HTML | Latex | HTML,Latex', type=str,
@@ -65,8 +81,38 @@ def parse_command_line(args, description):
 def _main_func():
     logger.addHandler(logging.StreamHandler())
     args = parse_command_line(sys.argv[1:], __doc__)
-    check_for_existing_file(args.host_pathnames, 'Host pathnames file')
-    check_for_existing_file(args.scheme_pathnames, 'Schemes pathnames file')
+    verbosity = args.verbose
+    host_pathsfile = args.host_pathnames
+    schemes_pathsfile = args.scheme_pathnames
+    sdf_pathsfile = args.sdf_pathlist
+    cap_output_file = args.cap_pathlist
+    host_output_file = args.host_pathlist
+    output_dir = os.path.abspath(args.output_root)
+    gen_docfiles = args.generate_docfiles
+    ## A few sanity checks
+    # Check required arguments
+    check_for_existing_file(host_pathsfile, 'Host pathnames file')
+    check_for_existing_file(schemes_pathsfile, 'Schemes pathnames file')
+    ## Make sure output directory is legit
+    if os.path.exists(output_dir):
+        if not os.path.isdir(output_dir):
+            logger.error("output-root, '{}', is not a directory".format(args.output_root))
+        elif not os.access(output_dir, os.W_OK):
+            logger.error("Cannot write files to output-root ({})".format(args.output_root))
+        # End if (output_dir is okay)
+    else:
+        # Try to create output_dir (let it crash if it fails)
+        os.makedirs(output_dir)
+    # End if
+    # Make sure we can create output file lists
+    if not os.path.isabs(cap_output_file):
+        cap_output_file = os.path.join(output_dir, cap_output_file)
+    # End if
+    check_for_writeable_file(cap_output_file, "Cap output file")
+    if not os.path.isabs(host_output_file):
+        host_output_file = os.path.join(output_dir, host_output_file)
+    # End if
+    check_for_writeable_file(host_output_file, "Host output file")
 
 ###############################################################################
 
