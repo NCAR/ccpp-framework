3790254083b085a92102413a6f0258c9ae80965d
Steve Goldhaber <goldy@ucar.edu>
2018-12-04 17:31:53 -0700

    Added module to schema, trying to get schema to validate from pytyhon

--- scripts/host_registry.py
+++ scripts/host_registry.py
@@ -19,7 +19,6 @@ except ImportError as ie:
     xmllint = None
 
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
 
 ###############################################################################
 class HostRegAbort(ValueError):
@@ -28,61 +27,123 @@ class HostRegAbort(ValueError):
         super(HostRegAbort, self).__init__(message)
 
 ###############################################################################
-def abort(message):
+def abort(message, log_error=True):
 ###############################################################################
-    logger.error(message)
+    if log_error:
+        logger.error(message)
+    # End if (no else)
     raise HostRegAbort(message)
 
 ###############################################################################
-def check_output(commands, verbose=False):
+def call_command(commands, silent=False):
 ###############################################################################
     """
     Try a command line and return the output on success (None on failure)
+    >>> call_command(['ls', 'really__improbable_fffilename.foo']) #doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    HostRegAbort: Execution of 'ls really__improbable_fffilename.foo' failed:
+    [Errno 2] No such file or directory
+    >>> call_command(['ls', 'really__improbable_fffilename.foo'], silent=True)
+    False
+    >>> call_command(['ls'])
+    True
     """
+    result = False
     try:
-        outstr = subprocess.check_output(commands, stderr=open("/dev/null", mode='w'))
-    except OSError as e:
-        logger.error("Execution of '{}' failed:".format(' '.join(commands)))
-        abort("{}".format(e), file=sys.stderr)
-    except ValueError as e:
-        if (verbose):
-            print("ValueError in '{}':".format(' '.join(commands)),
-              file=sys.stderr)
-            print("{}".format(e), file=sys.stderr)
-            # End if
-            outstr = None
-    except subprocess.CalledProcessError as e:
-        if (verbose):
-            print("CalledProcessError in '{}':".format(' '.join(commands)),
-              file=sys.stderr)
-            print("{}".format(e), file=sys.stderr)
+        cmd = ' '.join(commands)
+        outstr = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        result = True
+    except (OSError, ValueError, subprocess.CalledProcessError) as err:
+        if silent:
+            outstr = ""
+        else:
+            outstr = "Execution of '{}' failed:\n".format(cmd)
+            outstr = outstr + "{}".format(err)
+            abort(outstr)
         # End if
-        outstr = None
     # End of try
-    return outstr
+    return result
 
 ###############################################################################
-def validate_xml_file(filename, schema):
+def find_host_version(root):
 ###############################################################################
     """
-    validate an XML file against a provided schema file using pylint
+    Find the version of the host registry file represented by root
+    >>> find_host_version(ET.fromstring('<model name="CAM" version="1.0"></model>'))
+    [1, 0]
+    >>> find_host_version(ET.fromstring('<model name="CAM" version="1.a"></model>')) #doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    HostRegAbort: Illegal version string, '1.a'
+    Format must be <integer>.<integer>
+    >>> find_host_version(ET.fromstring('<model name="CAM" version="0.0"></model>')) #doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    HostRegAbort: Illegal version string, '0.0'
+    Major version must be at least 1
+    >>> find_host_version(ET.fromstring('<model name="CAM" version="0.-1"></model>')) #doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    HostRegAbort: Illegal version string, '0.0'
+    Minor version must be at least 0
     """
+    verbits = None
+    if 'version' not in root.attrib:
+        abort("version attribute required")
+    else:
+        version = root.attrib['version']
+        versplit = version.split('.')
+        try:
+            if len(versplit) != 2:
+                raise ValueError('oops')
+            verbits = [int(x) for x in versplit]
+            if verbits[0] < 1:
+                raise ValueError('Major version must be at least 1')
+            elif verbits[1] < 0:
+                raise ValueError('Minor version must be non-negative')
+            # End if (no else needed)
+        except ValueError as ve:
+            errstr = """Illegal version string, '{}'
+Format must be <integer>.<integer>"""
+            ve_str = str(ve)
+            if len(ve_str) > 0:
+                errstr = ve_str + '\n' + errstr
+            # End if
+            abort(errstr.format(version))
+        # End try
+    # End if
+    return verbits
+
+###############################################################################
+def validate_xml_file(filename, version):
+###############################################################################
+    """
+    Find the appropriate schema and validate the XML file, <filename>,
+    against it using xmllint
+    """
+    # Check the filename
     if not os.path.isfile(filename):
         abort("read_xml_file: Filename, '{}', does not exist".format(filename))
     elif not os.access(filename, os.R_OK):
         abort("read_xml_file: Cannot open '{}'".format(filename))
     # End if
-    if not os.path.isfile(schema):
-        abort("read_xml_file: Schema, '{}', does not exist".format(schema))
-    elif not os.access(schema, os.R_OK):
-        abort("read_xml_file: Cannot open schema, '{}'".format(schema))
+    # Find the schema, based on the model version
+    pdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    schema_path = os.path.join(pdir, 'src')
+    verstring = '_'.join([str(x) for x in version])
+    schema_file = os.path.join(schema_path,
+                               "host_registry_v{}.xsd".format(verstring))
+    if not os.path.isfile(schema_file):
+        verstring = '.'.join([str(x) for x in version])
+        abort("""read_xml_file: Cannot find schema for version {},
+{} does not exist""".format(verstring, schema_file))
+    elif not os.access(schema_file, os.R_OK):
+        abort("read_xml_file: Cannot open schema, '{}'".format(schema_file))
     # End if
     if xmllint is not None:
-        logger.debug("Checking file {} against schema {}".format(filename, schema))
-        outstr = check_output([xmllint, '--noout', '--schema', schema, filename])
-        return outstr is not None
+        logger.debug("Checking file {} against schema {}".format(filename, schema_file))
+        result = call_command([xmllint, '--noout', '--schema', schema_file, filename])
+        return result
     else:
         logger.warning("xmllint not found, could not validate file {}".format(filename))
+        return True # We could not check but still need to proceed
 
 ###############################################################################
 def read_xml_file(filename):
@@ -104,14 +165,25 @@ def read_xml_file(filename):
     return tree, root
 
 ###############################################################################
-def _main_func():
-###############################################################################
-    pass
-
-###############################################################################
 
 if __name__ == "__main__":
+    logger.addHandler(logging.NullHandler())
     try:
-        _main_func()
+        # First, run doctest
+        import doctest
+        doctest.testmod()
+        # We are in a subdir, find framework root and host root
+        hdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        cam7 = os.path.join(hdir, "cam_driver", "src", "cam7_registry.xml")
+        if os.path.exists(cam7):
+            tree, root = read_xml_file(cam7)
+            version = find_host_version(root)
+            print("CAM7 registry version is {}".format(version))
+            res = validate_xml_file(cam7, version)
+            print("CAM7 registry {}".format("validates" if res else "does not validate"))
+        else:
+            print("Could not find CAM7 registry, {}".format(cam7))
     except HostRegAbort as ca:
-        pass # abort already logged the message
+        print("{}".format(ca))
+else:
+    logger.addHandler(logging.StreamHandler())
