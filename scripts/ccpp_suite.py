e97c8bc603ef4b0e263a98f6a66da82579ed5684
gold2718 <goldy@ucar.edu>
2018-12-13 23:54:18 -0700

    Use continue to print reasonable line lengths

--- scripts/ccpp_suite.py
+++ scripts/ccpp_suite.py
@@ -40,7 +40,10 @@ COPYRIGHT = '''!
 ! CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-INDENT = 3
+INDENT    =   3 # Spaces per indent level
+CONTINUE  =   5 # Extra spaces on continuation line
+LINE_FILL =  78 # Target line length
+LINE_MAX  = 130 # Max line length
 
 ###############################################################################
 
@@ -51,9 +54,77 @@ class SuiteAbort(ValueError):
 
 ###############################################################################
 
-def indent(level=0):
+def indent(level=0, continue_line=False):
     'Return an indent string for any level'
-    return (INDENT*level)*' '
+    if continue_line:
+        return ((INDENT*level)+CONTINUE)*' '
+    else:
+        return (INDENT*level)*' '
+    # End if
+
+###############################################################################
+
+def find_best_break(choices, last=LINE_FILL):
+    'Find largest good break'
+    possible = [x for x in choices if x < last]
+    if len(possible) == 0:
+        best = LINE_MAX + 1
+    else:
+        best = max(possible)
+    # End if
+    if (best > LINE_MAX) and (last < LINE_MAX):
+        best = find_best_break(choices, last=LINE_MAX)
+    # End if
+    return best
+
+###############################################################################
+
+def write_fortran(outfile, statement, indent_level, continue_line=False):
+    istr = indent(indent_level, continue_line)
+    outstr = istr + statement.strip()
+    line_len = len(outstr)
+    if line_len > LINE_FILL:
+        # Collect pretty break points
+        spaces = list()
+        commas = list()
+        sptr = len(istr)
+        in_single_char = False
+        in_double_char = False
+        while sptr < line_len:
+            if in_single_char:
+                if outstr[sptr] == "'":
+                    in_single_char = False
+                # End if (no else, just copy stuff in string)
+            elif in_double_char:
+                if outstr[sptr] == '"':
+                    in_double_char = False
+                # End if (no else, just copy stuff in string)
+            elif outstr[sptr] == "'":
+                in_single_char = True
+            elif outstr[sptr] == '"':
+                in_double_char = True
+            elif outstr[sptr] == '!':
+                # Commend in non-character context, suck in rest of line
+                sptr = line_len - 1
+            elif outstr[sptr] == ' ':
+                # Non-quote spaces are where we can break
+                spaces.append(sptr)
+            elif outstr[sptr] == ',':
+                # Non-quote commas are where we can break
+                commas.append(sptr)
+            # End if (no else, other characters will be ignored)
+            sptr = sptr + 1
+        # End while
+        best = find_best_break(spaces)
+        if best >= LINE_FILL:
+            best = find_best_break(commas)
+        # End if
+        outfile.write("{}&\n".format(outstr[0:best+1]))
+        statement = "  {}".format(outstr[best+1:])
+        write_fortran(outfile, statement, indent_level, continue_line=True)
+    else:
+        outfile.write("{}\n".format(outstr))
+    # End if
 
 ###############################################################################
 
@@ -93,7 +164,7 @@ class Scheme(object):
         return ''
 
     def write(self, outfile, level=2): # Add phase!!
-        outfile.write('{}call {}({})'.format(indent(level), self._subroutine_name, self._arglist))
+        write_fortran(outfile, 'call {}({})'.format(self._subroutine_name, self._arglist), level)
 
     def schemes(self):
         'Return self as a list for consistency with subcycle'
@@ -121,11 +192,11 @@ class Subcycle(object):
         return loopvar, scheme_mods
 
     def write(self, outfile):
-        outfile.write('{}do {} = 1, {}'.format(indent(2), self.name, self.loop))
+        write_fortran(outfile, 'do {} = 1, {}'.format(self.name, self.loop), 2)
         for scheme in self._schemes:
             scheme.write(outfile, level=3)
         # End for
-        outfile.write('{}end do'.format(indent(2)))
+        write_fortran(outfile, 'end do', 2)
 
     @property
     def name(self):
@@ -228,20 +299,20 @@ class Group(object):
         local_subs = ''
         # First, write out the subroutine header
         subname = self.name + '_run'
-        outfile.write(Group.subhead.format(subname=subname, args=host_arglist))
+        write_fortran(outfile, Group.subhead.format(subname=subname, args=host_arglist), 1)
         # Write out the scheme use statements
         for scheme in self._local_schemes:
-            outfile.write(scheme)
+            write_fortran(outfile, scheme, 1)
         # End for
         # Write out local variables
         for var in self._local_vars:
-            outfile.write(var)
+            write_fortran(outfile, var, 1)
         # End for
         # Write the scheme calls
         for item in self._parts:
             item.write(outfile)
         # End for
-        outfile.write(Group.subend.format(subname=subname))
+        write_fortran(outfile, Group.subend.format(subname=subname), 1)
             # # Test and set blocks for initialization status
             # initialized_test_block = Group.initialized_test_blocks[ccpp_stage].format(
             #                             target_name_flag=ccpp_error_flag_target_name,
