9675a438aeaef69fca80d29086613ae5846e3365
Dom Heinzeller <climbfuji@ymail.com>
2020-03-25 05:55:06 -0600

    Python3 compatibility for CCPP prebuild scripts

--- scripts/conversion_tools/__init__.py
+++ scripts/conversion_tools/__init__.py
@@ -2,7 +2,49 @@
 """
 
 __all__ = [
-    'units',
+    'mm__to__m',
+    'm__to__mm',
+    'um__to__m',
+    'm__to__um',
+    'm__to__km',
+    'km__to__m',
+    'mm__to__km',
+    'km__to__mm',
+    's__to__min',
+    'min__to__s',
+    's__to__h',
+    'h__to__s',
+    'h__to__d',
+    'd__to__h',
+    's__to__d',
+    'd__to__s',
+    'Pa__to__hPa',
+    'hPa__to__Pa',
+    'm_s_minus_1__to__km_h_minus_1',
+    'km_h_minus_1__to__m_s_minus_1',
+    'W_m_minus_2__to__erg_cm_minus_2_s_minus_1',
+    'erg_cm_minus_2_s_minus_1__to__W_m_minus_2',
     ]
 
-import unit_conversion
+from .unit_conversion import mm__to__m
+from .unit_conversion import m__to__mm
+from .unit_conversion import um__to__m
+from .unit_conversion import m__to__um
+from .unit_conversion import m__to__km
+from .unit_conversion import km__to__m
+from .unit_conversion import mm__to__km
+from .unit_conversion import km__to__mm
+from .unit_conversion import s__to__min
+from .unit_conversion import min__to__s
+from .unit_conversion import s__to__h
+from .unit_conversion import h__to__s
+from .unit_conversion import h__to__d
+from .unit_conversion import d__to__h
+from .unit_conversion import s__to__d
+from .unit_conversion import d__to__s
+from .unit_conversion import Pa__to__hPa
+from .unit_conversion import hPa__to__Pa
+from .unit_conversion import m_s_minus_1__to__km_h_minus_1
+from .unit_conversion import km_h_minus_1__to__m_s_minus_1
+from .unit_conversion import W_m_minus_2__to__erg_cm_minus_2_s_minus_1
+from .unit_conversion import erg_cm_minus_2_s_minus_1__to__W_m_minus_2
