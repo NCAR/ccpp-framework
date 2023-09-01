73ef0a397058be29b1ad4c7a3bfd0f8d73f689a5
Courtney Peverley <courtneyp@cheyenne3.cheyenne.ucar.edu>
2023-09-01 10:19:26 -0600

    add ddt testing and update constituent standard names

---
+++ test/unit_tests/sample_host_files/ddt1.F90
@@ -0,0 +1,17 @@
+module ddt1
+
+   use ccpp_kinds, only: kind_phys
+
+   private
+   implicit none
+
+   !! \section arg_table_ddt1_t
+   !! \htmlinclude ddt1_t.html
+   !!
+   type, public :: ddt1_t
+      integer,                 private :: num_vars = 0
+      real(kind_phys), allocatable     :: vars(:,:,:)
+
+   end type ddt1_t
+
+end module ddt1
---
+++ test/unit_tests/sample_host_files/ddt_data1_mod.F90
@@ -0,0 +1,30 @@
+module ddt_data1_mod
+
+   use ccpp_kinds, only: kind_phys
+
+   private
+   implicit none
+
+   !! \section arg_table_ddt1_t
+   !! \htmlinclude ddt1_t.html
+   !!
+   type, public :: ddt1_t
+      real, pointer :: undocumented_array(:) => NULL()
+   end type ddt1_t
+
+   !! \section arg_table_ddt2_t
+   !! \htmlinclude ddt2_t.html
+   !!
+   type, public :: ddt2_t
+      integer,                 private :: num_vars = 0
+      real(kind_phys), allocatable     :: vars(:,:,:)
+
+   end type ddt2_t
+
+   !> \section arg_table_ddt_data1_mod  Argument Table
+   !! \htmlinclude arg_table_ddt_data1_mod.html
+   real(kind_phys)              :: ps1
+   real(kind_phys), allocatable :: xbox(:,:)
+   real(kind_phys), allocatable :: switch(:,:)
+
+end module ddt_data1_mod
