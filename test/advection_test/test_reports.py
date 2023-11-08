29ee9ae3bf3fb6fcf6153526ea7efda78a85ce01
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-11-08 09:55:32 -0700

    For advection tests, add --debug flag to capgen and update test reports

--- test/advection_test/test_reports.py
+++ test/advection_test/test_reports.py
@@ -72,13 +72,19 @@ _REQUIRED_VARS_CLD = ["ccpp_error_code", "ccpp_error_message",
                       "time_step_for_physics", "water_temperature_at_freezing",
                       "water_vapor_specific_humidity",
                       "cloud_ice_dry_mixing_ratio",
-                      "cloud_liquid_dry_mixing_ratio"]
+                      "cloud_liquid_dry_mixing_ratio",
+                      # Added by --debug option
+                      "horizontal_dimension",
+                      "vertical_layer_dimension"]
 _INPUT_VARS_CLD = ["surface_air_pressure", "temperature",
                    "horizontal_loop_begin", "horizontal_loop_end",
                    "time_step_for_physics", "water_temperature_at_freezing",
                    "water_vapor_specific_humidity",
                    "cloud_ice_dry_mixing_ratio",
-                   "cloud_liquid_dry_mixing_ratio"]
+                   "cloud_liquid_dry_mixing_ratio",
+                   # Added by --debug option
+                   "horizontal_dimension",
+                   "vertical_layer_dimension"]
 _OUTPUT_VARS_CLD = ["ccpp_error_code", "ccpp_error_message",
                     "water_vapor_specific_humidity", "temperature",
                     "cloud_ice_dry_mixing_ratio",
