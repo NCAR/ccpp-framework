b1454853fbac665dac7038e6055aef66f9a114d8
Dustin Swales <dustin.swales@noaa.gov>
2024-01-25 22:47:28 +0000

    Address reviewers comments

--- test/var_compatibility_test/effr_calc.F90
+++ test/var_compatibility_test/effr_calc.F90
@@ -16,7 +16,7 @@ contains
    !! \htmlinclude arg_table_effr_calc_run.html
    !!
    subroutine effr_calc_run(ncol, nlev, effrr_in, effrg_in, effrl_inout, &
-                            effri_out, effrs_inout, errmsg, errflg)
+                            effri_out, effrs_inout, has_graupel, errmsg, errflg)
 
       integer,            intent(in)    :: ncol
       integer,            intent(in)    :: nlev
@@ -25,6 +25,7 @@ contains
       real(kind_phys),    intent(inout) :: effrl_inout(:,:)
       real(kind_phys),    intent(out)   :: effri_out(:,:)
       real(8),intent(inout) :: effrs_inout(:,:)
+      logical,            intent(in)    :: has_graupel
       character(len=512), intent(out)   :: errmsg
       integer,            intent(out)   :: errflg
       !----------------------------------------------------------------
@@ -39,7 +40,7 @@ contains
       errflg = 0
 
       effrr_local = effrr_in
-      effrg_local = effrg_in
+      if (has_graupel) effrg_local = effrg_in
       effrl_inout = min(max(effrl_inout,re_qc_min),re_qc_max)
       effri_out   = re_qi_avg
       effrs_inout = effrs_inout + 10.0 ! in micrometer
