6556d35c80e98fc3bc23320790faa4e60bb4cfbd
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-03-22 09:17:03 -0600

    Add wrapper to run all prebuild tests

---
+++ test_prebuild/run_all_tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "" && echo "Running unit_tests       " && cd unit_tests        && ./run_tests.sh && cd ..
+echo "" && echo "Running test_opt_arg     " && cd test_opt_arg      && ./run_test.sh  && cd ..
+echo "" && echo "Running test_blocked_data" && cd test_blocked_data && ./run_test.sh  && cd ..
+echo "" && echo "Running test_chunked_data" && cd test_chunked_data && ./run_test.sh  && cd ..
