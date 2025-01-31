d536aa7c73b624e6067bbbe284778b64f668e699
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2025-01-30 19:21:46 -0700

    Attempt consolidating of cmake.

--- cmake/ccpp_capgen.cmake
+++ cmake/ccpp_capgen.cmake
@@ -1,45 +1,46 @@
 function(ccpp_capgen)
   set(optionalArgs CAPGEN_DEBUG CAPGEN_EXPECT_THROW_ERROR)
-  set(oneValueArgs HOSTFILES SCHEMEFILES SUITES HOST_NAME OUTPUT_ROOT VERBOSITY)
+  set(oneValueArgs HOST_NAME OUTPUT_ROOT VERBOSITY)
+  set(multi_value_keywords HOSTFILES SCHEMEFILES SUITES)
 
-  cmake_parse_arguments(arg "${optionalArgs}" "${oneValueArgs}" "" ${ARGN})
+  cmake_parse_arguments(arg "${optionalArgs}" "${oneValueArgs}" "${multi_value_keywords}" ${ARGN})
 
-  list(APPEND CCPP_CAPGEN_CMD "${CMAKE_SOURCE_DIR}/scripts/ccpp_capgen.py")
+  list(APPEND CCPP_CAPGEN_CMD_LIST "${CMAKE_SOURCE_DIR}/scripts/ccpp_capgen.py")
 
   if(DEFINED arg_CAPGEN_DEBUG)
-    list(APPEND CCPP_CAPGEN_CMD "--debug")
+    list(APPEND CCPP_CAPGEN_CMD_LIST "--debug")
   endif()
 
   if(DEFINED arg_HOSTFILES)
     list(JOIN arg_HOSTFILES "," HOSTFILES_SEPARATED)
-    list(APPEND CCPP_CAPGEN_CMD "--host-files" "${HOSTFILES_SEPARATED}")
+    list(APPEND CCPP_CAPGEN_CMD_LIST "--host-files" "${HOSTFILES_SEPARATED}")
   endif()
   if(DEFINED arg_SCHEMEFILES)
     list(JOIN arg_SCHEMEFILES "," SCHEMEFILES_SEPARATED)
-    list(APPEND CCPP_CAPGEN_CMD "--scheme-files" "${SCHEMEFILES_SEPARATED}")
+    list(APPEND CCPP_CAPGEN_CMD_LIST "--scheme-files" "${SCHEMEFILES_SEPARATED}")
   endif()
   if(DEFINED arg_SUITES)
     list(JOIN arg_SUITES "," SUITES_SEPARATED)
-    list(APPEND CCPP_CAPGEN_CMD "--suites" "${SUITES_SEPARATED}")
+    list(APPEND CCPP_CAPGEN_CMD_LIST "--suites" "${SUITES_SEPARATED}")
   endif()
   if(DEFINED arg_HOST_NAME)
-    list(APPEND CCPP_CAPGEN_CMD "--host-name" "${arg_HOST_NAME}")
+    list(APPEND CCPP_CAPGEN_CMD_LIST "--host-name" "${arg_HOST_NAME}")
   endif()
   if(DEFINED arg_OUTPUT_ROOT)
     message(STATUS "Creating output directory: ${arg_OUTPUT_ROOT}")
     file(MAKE_DIRECTORY "${arg_OUTPUT_ROOT}")
-    list(APPEND CCPP_CAPGEN_CMD "--output-root" "${arg_OUTPUT_ROOT}")
+    list(APPEND CCPP_CAPGEN_CMD_LIST "--output-root" "${arg_OUTPUT_ROOT}")
   endif()
   if(DEFINED arg_VERBOSITY)
     string(REPEAT "--verbose" ${arg_VERBOSITY} VERBOSE_PARAMS_SEPERATED)
     separate_arguments(VERBOSE_PARAMS UNIX_COMMAND "${VERBOSE_PARAMS_SEPERATED}")
-    list(APPEND CCPP_CAPGEN_CMD ${VERBOSE_PARAMS})
+    list(APPEND CCPP_CAPGEN_CMD_LIST ${VERBOSE_PARAMS})
   endif()
 
   message(STATUS "Running ccpp_capgen from ${CMAKE_CURRENT_SOURCE_DIR}")
 
-  list(JOIN CAPGEN_CMD_PARAMS_LIST " " CCPP_CAPGEN_CMD)
-  message(STATUS "Running ccpp_capgen: ${CAPGEN_CMD_PARAMS_LIST}")
+  list(JOIN CCPP_CAPGEN_CMD_LIST " " CCPP_CAPGEN_CMD)
+  message(STATUS "Running ccpp_capgen: ${CCPP_CAPGEN_CMD}")
 
   execute_process(COMMAND ${CCPP_CAPGEN_CMD}
                   WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
@@ -47,7 +48,7 @@ function(ccpp_capgen)
                   ERROR_VARIABLE CAPGEN_OUT
                   RESULT_VARIABLE RES)
 
-  message(STATUS "ccpp-capgen stdout:" ${CAPGEN_OUT})
+  message(STATUS "ccpp-capgen stdout: ${CAPGEN_OUT}")
 
   if(arg_CAPGEN_EXPECT_THROW_ERROR)
     string(FIND "${CAPGEN_OUT}" "Variables of type ccpp_constituent_properties_t only allowed in register phase" ERROR_INDEX)
