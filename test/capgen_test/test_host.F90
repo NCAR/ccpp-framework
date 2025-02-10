8155f8176bb05cc72cb517a03b1eb60f44cc49cb
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2025-02-10 08:21:32 -0700

    Initial test_utils integration.

--- test/capgen_test/test_host.F90
+++ test/capgen_test/test_host.F90
@@ -24,92 +24,10 @@ module test_prog
 
 CONTAINS
 
-   logical function check_list(test_list, chk_list, list_desc, suite_name)
-      ! Check a list (<test_list>) against its expected value (<chk_list>)
-
-      ! Dummy arguments
-      character(len=*),           intent(in) :: test_list(:)
-      character(len=*),           intent(in) :: chk_list(:)
-      character(len=*),           intent(in) :: list_desc
-      character(len=*), optional, intent(in) :: suite_name
-
-      ! Local variables
-      logical                                :: found
-      integer                                :: num_items
-      integer                                :: lindex, tindex
-      integer,          allocatable          :: check_unique(:)
-      character(len=2)                       :: sep
-      character(len=256)                     :: errmsg
-
-      check_list = .true.
-      errmsg = ''
-
-      ! Check the list size
-      num_items = size(chk_list)
-      if (size(test_list) /= num_items) then
-         write(errmsg, '(a,i0,2a)') 'ERROR: Found ', size(test_list),         &
-              ' ', trim(list_desc)
-         if (present(suite_name)) then
-            write(errmsg(len_trim(errmsg)+1:), '(2a)') ' for suite, ',        &
-                 trim(suite_name)
-         end if
-         write(errmsg(len_trim(errmsg)+1:), '(a,i0)') ', should be ', num_items
-         write(6, *) trim(errmsg)
-         errmsg = ''
-         check_list = .false.
-       end if
-
-       ! Now, check the list contents for 1-1 correspondence
-       if (check_list) then
-          allocate(check_unique(num_items))
-          check_unique = -1
-          do lindex = 1, num_items
-             found = .false.
-             do tindex = 1, num_items
-                if (trim(test_list(lindex)) == trim(chk_list(tindex))) then
-                   check_unique(tindex) = lindex
-                   found = .true.
-                   exit
-                end if
-             end do
-             if (.not. found) then
-                check_list = .false.
-                write(errmsg, '(5a)') 'ERROR: ', trim(list_desc), ' item, ',  &
-                     trim(test_list(lindex)), ', was not found'
-                if (present(suite_name)) then
-                   write(errmsg(len_trim(errmsg)+1:), '(2a)') ' in suite, ',  &
-                        trim(suite_name)
-                end if
-                write(6, *) trim(errmsg)
-                errmsg = ''
-             end if
-          end do
-          if (check_list .and. ANY(check_unique < 0)) then
-             check_list = .false.
-             write(errmsg, '(3a)') 'ERROR: The following ', trim(list_desc),  &
-                  ' items were not found'
-             if (present(suite_name)) then
-                write(errmsg(len_trim(errmsg)+1:), '(2a)') ' in suite, ',     &
-                     trim(suite_name)
-             end if
-             sep = '; '
-             do lindex = 1, num_items
-                if (check_unique(lindex) < 0) then
-                   write(errmsg(len_trim(errmsg)+1:), '(2a)') sep,            &
-                        trim(chk_list(lindex))
-                   sep = ', '
-                end if
-             end do
-             write(6, *) trim(errmsg)
-             errmsg = ''
-          end if
-       end if
-
-    end function check_list
-
     logical function check_suite(test_suite)
        use test_host_ccpp_cap, only: ccpp_physics_suite_part_list
        use test_host_ccpp_cap, only: ccpp_physics_suite_variables
+       use test_utils,         only: check_list
 
        ! Dummy argument
        type(suite_info), intent(in)    :: test_suite
@@ -195,6 +113,7 @@ CONTAINS
        use test_host_ccpp_cap, only: test_host_ccpp_physics_finalize
        use test_host_ccpp_cap, only: ccpp_physics_suite_list
        use test_host_mod,      only: init_data, compare_data, check_model_times
+       use test_utils,         only: check_list
 
        type(suite_info), intent(in)  :: test_suites(:)
        logical,          intent(out) :: retval
