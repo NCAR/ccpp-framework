ac67b47f38c0a938e6f90b4b6cfcccad70b18797
Steve Goldhaber <stevenng@met.no>
2025-02-15 21:32:23 +0100

    Add (and check) check metadata file

--- test/test_fortran_to_metadata.sh
+++ test/test_fortran_to_metadata.sh
@@ -13,3 +13,25 @@ sample_meta="${sample_files}/check_fortran_to_metadata.meta"
 # Run the script
 opts="--ddt-names serling_t"
 ${f2m_script} --output-root "${tmp_dir}" ${opts} "${test_input}"
+res=$?
+
+retval=0
+if [ ${res} -ne 0 ]; then
+    echo "FAIL: ccpp_fortran_to_metadata.py exited with error ${res}"
+    retval=${res}
+elif [ ! -f "${tmp_dir}/${filename}.meta" ]; then
+    echo "FAIL: metadata file, '${tmp_dir}/${filename}.meta', not created"
+    retval=1
+else
+    cmp --quiet "${sample_meta}" "${tmp_dir}/${filename}.meta"
+    res=$?
+    if [ ${res} -ne 0 ]; then
+        echo "FAIL: Comparison with correct metadata file failed"
+        retval=${res}
+    else
+        echo "PASS"
+        # Cleanup
+        rm "${tmp_dir}/${filename}.meta"
+    fi
+fi
+exit ${retval}
