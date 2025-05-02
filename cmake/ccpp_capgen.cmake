23f6ce73324dcdf64670474c1217a5ff5436c237
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2025-05-02 16:02:19 -0600

    Removing dead code.

--- cmake/ccpp_capgen.cmake
+++ cmake/ccpp_capgen.cmake
@@ -4,9 +4,7 @@ function(ccpp_capgen)
   set(multi_value_keywords HOSTFILES SCHEMEFILES SUITES)
 
   cmake_parse_arguments(arg "${optionalArgs}" "${oneValueArgs}" "${multi_value_keywords}" ${ARGN})
-
-  # list(APPEND CCPP_CAPGEN_CMD_LIST "${CMAKE_SOURCE_DIR}/scripts/ccpp_capgen.py")
-
+  
   unset(CCPP_CAPGEN_CMD_LIST)
   if(DEFINED arg_CAPGEN_DEBUG)
     list(APPEND CCPP_CAPGEN_CMD_LIST "--debug")
