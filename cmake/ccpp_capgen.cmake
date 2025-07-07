5a4b60b6fb756bdf01107caf992a735b8c129fb2
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2025-07-07 15:04:40 -0600

    Documentation update/cleanup.

--- cmake/ccpp_capgen.cmake
+++ cmake/ccpp_capgen.cmake
@@ -1,3 +1,14 @@
+# CMake wrapper for ccpp_capgen.py
+# Currently meant to be a CMake API needed for generating caps for regression tests.
+#
+# CAPGEN_DEBUG              - ON/OFF (Default: OFF) - Enables debug capability through ccpp_capgen.py
+# CAPGEN_EXPECT_THROW_ERROR - ON/OFF (Default: OFF) - Scans ccpp_capgen.py log for error string and errors if not found.
+# HOST_NAME                 - String name of host
+# OUTPUT_ROOT               - String path to put generated caps
+# VERBOSITY                 - Number of --verbose flags to pass to capgen
+# HOSTFILES                 - CMake list of host metadata filenames
+# SCHEMEFILES               - CMake list of scheme metadata files
+# SUITES                    - CMake list of suite xml files
 function(ccpp_capgen)
   set(optionalArgs CAPGEN_DEBUG CAPGEN_EXPECT_THROW_ERROR)
   set(oneValueArgs HOST_NAME OUTPUT_ROOT VERBOSITY)
@@ -5,11 +16,16 @@ function(ccpp_capgen)
 
   cmake_parse_arguments(arg "${optionalArgs}" "${oneValueArgs}" "${multi_value_keywords}" ${ARGN})
 
-  unset(CCPP_CAPGEN_CMD_LIST)
+  # Error if script file not found.
+  set(CCPP_CAPGEN_CMD_LIST "${CMAKE_SOURCE_DIR}/scripts/ccpp_capgen.py")
+  if(NOT EXISTS ${CCPP_CAPGEN_CMD_LIST})
+    message(FATAL_ERROR "function(ccpp_capgen): Could not find ccpp_capgen.py.  Looked for ${CCPP_CAPGEN_CMD_LIST}.")
+  endif()
+
+  # Interpret parsed arguments
   if(DEFINED arg_CAPGEN_DEBUG)
     list(APPEND CCPP_CAPGEN_CMD_LIST "--debug")
   endif()
-
   if(DEFINED arg_HOSTFILES)
     list(JOIN arg_HOSTFILES "," HOSTFILES_SEPARATED)
     list(APPEND CCPP_CAPGEN_CMD_LIST "--host-files" "${HOSTFILES_SEPARATED}")
@@ -36,22 +52,20 @@ function(ccpp_capgen)
     list(APPEND CCPP_CAPGEN_CMD_LIST ${VERBOSE_PARAMS})
   endif()
 
-  message(STATUS "Running ccpp_capgen from ${CMAKE_CURRENT_SOURCE_DIR}")
+  message(STATUS "Running ccpp_capgen.py from ${CMAKE_CURRENT_SOURCE_DIR}")
 
-  list(JOIN CCPP_CAPGEN_CMD_LIST " " CCPP_CAPGEN_CMD)
-  message(STATUS "Running ccpp_capgen with arguments: ${CCPP_CAPGEN_CMD}")
-
-  list(JOIN CCPP_CAPGEN_CMD_LIST ";" CCPP_CAPGEN_CMAKE_CMD)
-  unset(CAPGEN_OUT)
-  execute_process(COMMAND "${CMAKE_SOURCE_DIR}/scripts/ccpp_capgen.py" ${CCPP_CAPGEN_CMAKE_CMD}
+  unset(CAPGEN_OUT) # Unset CAPGEN_OUT to prevent incorrect output on subsequent calls.
+  execute_process(COMMAND ${CCPP_CAPGEN_CMD_LIST}
                   WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
                   OUTPUT_VARIABLE CAPGEN_OUT
                   ERROR_VARIABLE CAPGEN_OUT
-                  RESULT_VARIABLE RES)
+                  RESULT_VARIABLE RES
+                  COMMAND_ECHO STDOUT)
 
   message(STATUS "ccpp-capgen stdout: ${CAPGEN_OUT}")
 
   if(arg_CAPGEN_EXPECT_THROW_ERROR)
+    # Determine if the process succeeded but had an expected string in the process log.
     string(FIND "${CAPGEN_OUT}" "Variables of type ccpp_constituent_properties_t only allowed in register phase" ERROR_INDEX)
 
     if (ERROR_INDEX GREATER -1)
@@ -68,47 +82,48 @@ function(ccpp_capgen)
   endif()
 endfunction()
 
-
-
+# CMake wrapper for ccpp_datafile.py
+# Currently meant to be a CMake API needed for generating caps for regression tests.
+#
+# DATATABLE   - Path to generated datatable.xml file
+# REPORT_NAME - String report name to get list of generated files form capgen (typically --ccpp-files)
 function(ccpp_datafile)
-  set(oneValueArgs DATATABLE REPORT_NAME CCPP_CAPS_LIB_FILES)
+  set(oneValueArgs DATATABLE REPORT_NAME)
   cmake_parse_arguments(arg "" "${oneValueArgs}" "" ${ARGN})
 
   set(CCPP_DATAFILE_CMD "${CMAKE_SOURCE_DIR}/scripts/ccpp_datafile.py")
+
   if(NOT EXISTS ${CCPP_DATAFILE_CMD})
     message(FATAL_ERROR "function(ccpp_datafile): Could not find ccpp_datafile.py.  Looked for ${CCPP_DATAFILE_CMD}.")
   endif()
-  if(NOT DEFINED arg_DATATABLE)
-    message(FATAL_ERROR "function(ccpp_datafile): DATATABLE not set.  A datatable file must be configured to call ccpp_datafile.")
-  endif()
-  list(APPEND CCPP_DATAFILE_CMD "${arg_DATATABLE}")
 
   if(NOT DEFINED arg_REPORT_NAME)
     message(FATAL_ERROR "function(ccpp_datafile): REPORT_NAME not set.  Must specify the report to generate to run cpp_datafile.py")
   endif()
   list(APPEND CCPP_DATAFILE_CMD "${arg_REPORT_NAME}")
 
-  message(STATUS "${CCPP_DATAFILE_CMD}")
+  if(NOT DEFINED arg_DATATABLE)
+    message(FATAL_ERROR "function(ccpp_datafile): DATATABLE not set.  A datatable file must be configured to call ccpp_datafile.")
+  endif()
+  list(APPEND CCPP_DATAFILE_CMD "${arg_DATATABLE}")
+
   message(STATUS "Running ccpp_datafile from ${CMAKE_CURRENT_SOURCE_DIR}")
 
-  string(REPLACE ";" " " CCPP_DATAFILE_CMD_SEPERATED "${CCPP_DATAFILE_CMD}")
-  message(STATUS "Running ccpp_datafile.py command: ${CCPP_DATAFILE_CMD_SEPERATED}")
-
   unset(CCPP_CAPS)
-  list(JOIN CCPP_DATAFILE_CMD ";" CCPP_DATAFILE_CMAKE_CMD)
-  execute_process(COMMAND ${CCPP_DATAFILE_CMAKE_CMD}
+  execute_process(COMMAND ${CCPP_DATAFILE_CMD}
                   WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
                   OUTPUT_VARIABLE CCPP_CAPS
                   RESULT_VARIABLE RES
                   OUTPUT_STRIP_TRAILING_WHITESPACE
-                  ERROR_STRIP_TRAILING_WHITESPACE)
+                  ERROR_STRIP_TRAILING_WHITESPACE
+                  COMMAND_ECHO STDOUT)
   message(STATUS "CCPP_CAPS = ${CCPP_CAPS}")
   if(RES EQUAL 0)
     message(STATUS "CCPP cap files retrieved")
   else()
     message(FATAL_ERROR "CCPP cap file retrieval FAILED: result = ${RES}")
   endif()
-  string(REPLACE "," ";" CCPP_CAPS_LIST ${CCPP_CAPS})
+  string(REPLACE "," ";" CCPP_CAPS_LIST ${CCPP_CAPS}) # Convert "," separated list from python back to ";" separated list for CMake.
   set(CCPP_CAPS_LIST "${CCPP_CAPS_LIST}" PARENT_SCOPE)
 endfunction()
 
