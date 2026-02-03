14af9edaeb1aff97b3301f075cf64fb8f7ee5a81
dustinswales <dswales@ucar.edu>
2026-02-02 22:07:29 -0700

    Revert some changes

--- scripts/metadata_table.py
+++ scripts/metadata_table.py
@@ -983,15 +983,10 @@ class MetadataSection(ParseSource):
                             pval = pval_str
                             pname = 'ddt_type'
                         else:
-                            if registered_fortran_ddt_name(pval_str):
-                                pval = pval_str.lower()
-                                pname = 'ddt_type'
-                            else:
-                                errmsg = "Unknown DDT type, {}".format(pval_str)
-                                self.__pobj.add_syntax_err(errmsg)
-                                self.__section_valid = False
-                                var_ok = False
-                            # end if
+                            errmsg = "Unknown DDT type, {}".format(pval_str)
+                            self.__pobj.add_syntax_err(errmsg)
+                            self.__section_valid = False
+                            var_ok = False
                         # end if
                     else:
                         # Make sure this is a match
