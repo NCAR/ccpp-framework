d4005ee081ba73d3e9009d672c774aa496972015
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-03-23 10:59:37 -0600

    Add threading test to `capgen_test`; codee format `test/capgen_test/*.F90` (#722)
    1. Add threading test to `capgen_test`. This is exercised with `ctest`
    without any further changes, and it passes. I added a comparison of all
    averages to the `compare_data` call, which compares the `omp1` and
    `omp2` test runs. This is commit
    https://github.com/NCAR/ccpp-framework/pull/722/changes/900069b975fb7b1b41f94df55e4230eae071e169.
    While testing this, @peverwhee and I uncovered a bug (uninitialized
    variable, see
    https://github.com/NCAR/ccpp-framework/pull/722#issuecomment-4055148707)
    that I had to fix.
    2. Update Codee to 2025.4.8 and simplify
    `.github/workflows/fortran-formatting.yaml` (there was an actual error
    using the workflow that needed to be fixed). This is commit
    https://github.com/NCAR/ccpp-framework/pull/722/changes/a76f82c38c024e3b13564479ed541a5ae34bc0d2
    3. Codee format `test/capgen_test/*.F90`. This is commit
    https://github.com/NCAR/ccpp-framework/pull/722/changes/24223ffe773e98973b48ff4bdc984a72a1ed4b4d
    
    User interface changes?: No
    
    Issues: Working toward https://github.com/NCAR/ccpp-framework/issues/721
    
    Testing: all pass
      test removed: n/a
      unit tests: n/a
    system tests: existing capgen test now run with `OMP_NUM_THREADS=1` and
    `OMP_NUM_THREADS=2`
    manual testing: tested manually on my dev machine with spack-stack-2.1.0

--- test/capgen_test/test_capgen_host_integration.F90
+++ test/capgen_test/test_capgen_host_integration.F90
@@ -1,86 +1,86 @@
 program test
-    use test_prog, only: test_host, suite_info, cm, cs
+  use test_prog, only: test_host, suite_info, cm, cs
 
-    implicit none
+  implicit none
 
-   character(len=cs), target :: test_parts1(2) = (/ 'physics1        ',       &
-                                                    'physics2        ' /)
-   character(len=cs), target :: test_parts2(1) = (/ 'data_prep       ' /)
-   character(len=cm), target :: test_invars1(10) = (/                         &
-        'potential_temperature               ',                               &
-        'potential_temperature_at_interface  ',                               &
-        'coefficients_for_interpolation      ',                               &
-        'surface_air_pressure                ',                               &
-        'water_vapor_specific_humidity       ',                               &
-        'potential_temperature_increment     ',                               &
-        'soil_levels                         ',                               &
-        'temperature_at_diagnostic_levels    ',                               &
-        'time_step_for_physics               ',                               &
-        'array_variable_for_testing          ' /)
-   character(len=cm), target :: test_outvars1(10) = (/                        &
-        'potential_temperature               ',                               &
-        'potential_temperature_at_interface  ',                               &
-        'coefficients_for_interpolation      ',                               &
-        'surface_air_pressure                ',                               &
-        'water_vapor_specific_humidity       ',                               &
-        'soil_levels                         ',                               &
-        'temperature_at_diagnostic_levels    ',                               &
-        'ccpp_error_code                     ',                               &
-        'ccpp_error_message                  ',                               &
-        'array_variable_for_testing          ' /)
-   character(len=cm), target :: test_reqvars1(12) = (/                        &
-        'potential_temperature               ',                               &
-        'potential_temperature_at_interface  ',                               &
-        'coefficients_for_interpolation      ',                               &
-        'surface_air_pressure                ',                               &
-        'water_vapor_specific_humidity       ',                               &
-        'potential_temperature_increment     ',                               &
-        'time_step_for_physics               ',                               &
-        'soil_levels                         ',                               &
-        'temperature_at_diagnostic_levels    ',	      	      	              &
-        'ccpp_error_code                     ',                               &
-        'ccpp_error_message                  ',                               &
-        'array_variable_for_testing          ' /)
+  character(len=cs), target :: test_parts1(2) = (/ 'physics1        ', &
+      'physics2        ' /)
+  character(len=cs), target :: test_parts2(1) = (/ 'data_prep       ' /)
+  character(len=cm), target :: test_invars1(10) = (/ &
+      'potential_temperature               ', &
+      'potential_temperature_at_interface  ', &
+      'coefficients_for_interpolation      ', &
+      'surface_air_pressure                ', &
+      'water_vapor_specific_humidity       ', &
+      'potential_temperature_increment     ', &
+      'soil_levels                         ', &
+      'temperature_at_diagnostic_levels    ', &
+      'time_step_for_physics               ', &
+      'array_variable_for_testing          ' /)
+  character(len=cm), target :: test_outvars1(10) = (/ &
+      'potential_temperature               ', &
+      'potential_temperature_at_interface  ', &
+      'coefficients_for_interpolation      ', &
+      'surface_air_pressure                ', &
+      'water_vapor_specific_humidity       ', &
+      'soil_levels                         ', &
+      'temperature_at_diagnostic_levels    ', &
+      'ccpp_error_code                     ', &
+      'ccpp_error_message                  ', &
+      'array_variable_for_testing          ' /)
+  character(len=cm), target :: test_reqvars1(12) = (/ &
+      'potential_temperature               ', &
+      'potential_temperature_at_interface  ', &
+      'coefficients_for_interpolation      ', &
+      'surface_air_pressure                ', &
+      'water_vapor_specific_humidity       ', &
+      'potential_temperature_increment     ', &
+      'time_step_for_physics               ', &
+      'soil_levels                         ', &
+      'temperature_at_diagnostic_levels    ', &
+      'ccpp_error_code                     ', &
+      'ccpp_error_message                  ', &
+      'array_variable_for_testing          ' /)
 
-   character(len=cm), target :: test_invars2(3) = (/                          &
-        'model_times                         ',                               &
-        'number_of_model_times               ',                               &
-        'surface_air_pressure                ' /)
+  character(len=cm), target :: test_invars2(3) = (/ &
+      'model_times                         ', &
+      'number_of_model_times               ', &
+      'surface_air_pressure                ' /)
 
-   character(len=cm), target :: test_outvars2(5) = (/                         &
-        'ccpp_error_code                     ',                               &
-        'ccpp_error_message                  ',                               &
-        'model_times                         ',                               &
-        'surface_air_pressure                ',                               &
-        'number_of_model_times               ' /)
+  character(len=cm), target :: test_outvars2(5) = (/ &
+      'ccpp_error_code                     ', &
+      'ccpp_error_message                  ', &
+      'model_times                         ', &
+      'surface_air_pressure                ', &
+      'number_of_model_times               ' /)
 
-   character(len=cm), target :: test_reqvars2(5) = (/                         &
-        'model_times                         ',                               &
-        'number_of_model_times               ',                               &
-        'surface_air_pressure                ',                               &
-        'ccpp_error_code                     ',                               &
-        'ccpp_error_message                  ' /)
-    type(suite_info) :: test_suites(2)
-    logical :: run_okay
+  character(len=cm), target :: test_reqvars2(5) = (/ &
+      'model_times                         ', &
+      'number_of_model_times               ', &
+      'surface_air_pressure                ', &
+      'ccpp_error_code                     ', &
+      'ccpp_error_message                  ' /)
+  type(suite_info) :: test_suites(2)
+  logical :: run_okay
 
-    ! Setup expected test suite info
-    test_suites(1)%suite_name = 'temp_suite'
-	test_suites(1)%suite_parts => test_parts1
-    test_suites(1)%suite_input_vars => test_invars1
-    test_suites(1)%suite_output_vars => test_outvars1
-    test_suites(1)%suite_required_vars => test_reqvars1
-    test_suites(2)%suite_name = 'ddt_suite'
-	test_suites(2)%suite_parts => test_parts2
-    test_suites(2)%suite_input_vars => test_invars2
-    test_suites(2)%suite_output_vars => test_outvars2
-    test_suites(2)%suite_required_vars => test_reqvars2
+  ! Setup expected test suite info
+  test_suites(1)%suite_name = 'temp_suite'
+  test_suites(1)%suite_parts => test_parts1
+  test_suites(1)%suite_input_vars => test_invars1
+  test_suites(1)%suite_output_vars => test_outvars1
+  test_suites(1)%suite_required_vars => test_reqvars1
+  test_suites(2)%suite_name = 'ddt_suite'
+  test_suites(2)%suite_parts => test_parts2
+  test_suites(2)%suite_input_vars => test_invars2
+  test_suites(2)%suite_output_vars => test_outvars2
+  test_suites(2)%suite_required_vars => test_reqvars2
 
-    call test_host(run_okay, test_suites)
+  call test_host(run_okay, test_suites)
 
-    if (run_okay) then
-       STOP 0
-    else
-       STOP -1
-    end if
+  if (run_okay) then
+    stop 0
+  else
+    stop -1
+  end if
 
 end program test
