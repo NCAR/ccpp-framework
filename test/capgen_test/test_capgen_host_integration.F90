df3be4344b3e452892fce717cfd4130b85848cfd
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-04-23 20:19:21 -0600

    Pointer assignment now ok, but results are off because temp doesn't have the correct indices

--- test/capgen_test/test_capgen_host_integration.F90
+++ test/capgen_test/test_capgen_host_integration.F90
@@ -6,60 +6,61 @@ program test
       'physics2        ' /)
   character(len=cs), target :: test_parts2(1) = (/ 'data_prep       ' /)
   character(len=cm), target :: test_invars1(10) = (/ &
-      'array_variable_for_testing            ', & 
-      'coefficients_for_interpolation        ', & 
-      'physics_state_derived_type            ', & 
-      'potential_temperature                 ', & 
-      'potential_temperature_at_interface    ', & 
-      'temperature_at_diagnostic_levels      ', & 
-      'index_of_water_vapor_specific_humidity', & 
-      'do_cloud_fraction_adjustment          ', & 
-      'potential_temperature_increment       ', & 
-      'time_step_for_physics                 '/)
+      'array_variable_for_testing             ', & 
+      'coefficients_for_interpolation         ', & 
+      'physics_state_derived_type             ', & 
+      'potential_temperature                  ', & 
+      'potential_temperature_at_interface     ', & 
+      'temperature_at_diagnostic_levels       ', & 
+      'index_of_water_vapor_specific_humidity ', & 
+      'do_cloud_fraction_adjustment           ', & 
+      'potential_temperature_increment        ', & 
+      'time_step_for_physics                  '/)
 
   character(len=cm), target :: test_outvars1(9) = (/ &
-      'array_variable_for_testing            ', &
-      'coefficients_for_interpolation        ', &
-      'physics_state_derived_type            ', &
-      'potential_temperature                 ', &
-      'potential_temperature_at_interface    ', &
-      'temperature_at_diagnostic_levels      ', &
-      'index_of_water_vapor_specific_humidity', &
-      'ccpp_error_code                       ', &
-      'ccpp_error_message                    '/)
+      'array_variable_for_testing             ', &
+      'coefficients_for_interpolation         ', &
+      'physics_state_derived_type             ', &
+      'potential_temperature                  ', &
+      'potential_temperature_at_interface     ', &
+      'temperature_at_diagnostic_levels       ', &
+      'index_of_water_vapor_specific_humidity ', &
+      'ccpp_error_code                        ', &
+      'ccpp_error_message                     '/)
 
   character(len=cm), target :: test_reqvars1(12) = (/ &
-      'array_variable_for_testing            ', &
-      'coefficients_for_interpolation        ', &
-      'physics_state_derived_type            ', &
-      'potential_temperature                 ', &
-      'potential_temperature_at_interface    ', &
-      'temperature_at_diagnostic_levels      ', &
-      'index_of_water_vapor_specific_humidity', &
-      'do_cloud_fraction_adjustment          ', &
-      'potential_temperature_increment       ', &
-      'time_step_for_physics                 ', &
-      'ccpp_error_code                       ', &
-      'ccpp_error_message                    '/)
+      'array_variable_for_testing             ', &
+      'coefficients_for_interpolation         ', &
+      'physics_state_derived_type             ', &
+      'potential_temperature                  ', &
+      'potential_temperature_at_interface     ', &
+      'temperature_at_diagnostic_levels       ', &
+      'index_of_water_vapor_specific_humidity ', &
+      'do_cloud_fraction_adjustment           ', &
+      'potential_temperature_increment        ', &
+      'time_step_for_physics                  ', &
+      'ccpp_error_code                        ', &
+      'ccpp_error_message                     '/)
 
   character(len=cm), target :: test_invars2(3) = (/ &
-      'model_times                         ', &
-      'number_of_model_times               ', &
-      'surface_air_pressure                ' /)
+      'model_times                            ', &
+      'number_of_model_times                  ', &
+      'physics_state_derived_type             ' /)
 
   character(len=cm), target :: test_outvars2(5) = (/ &
-      'ccpp_error_code                     ', &
-      'ccpp_error_message                  ', &
-      'model_times                         ', &
-      'surface_air_pressure                ', &
-      'number_of_model_times               ' /)
+      'number_of_model_times                  ', &
+      'physics_state_derived_type             ', &
+      'ccpp_error_code                        ', &
+      'ccpp_error_message                     ', &
+      'model_times                            ' /)
 
   character(len=cm), target :: test_reqvars2(5) = (/ &
-      'model_times                         ', &
-      'number_of_model_times               ', &
-      'surface_air_pressure                ', &
-      'ccpp_error_code                     ', &
-      'ccpp_error_message                  ' /)
+      'number_of_model_times                  ', &
+      'physics_state_derived_type             ', &
+      'model_times                            ', &
+      'ccpp_error_code                        ', &
+      'ccpp_error_message                     ' /)
+
   type(suite_info) :: test_suites(2)
   logical :: run_okay
 
