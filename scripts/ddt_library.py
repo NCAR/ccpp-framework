a730b349583e9f3c544491642c80cc52bbbfcf53
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-04-28 11:19:50 -0600

    scripts/ddt_library.py: add call_dimstring to VarDDT

--- scripts/ddt_library.py
+++ scripts/ddt_library.py
@@ -101,6 +101,27 @@ class VarDDT(Var):
         # end if
         return clone_var
 
+    def call_dimstring(self, var_dicts=None, explicit_dims=False,
+                       loop_subst=False, prepend_varname=False):
+        """Return the dimensions string for a variable call.
+        If <var_dict> is present, find and substitute a local_name for
+        each standard_name in this variable's dimensions.
+        If <var_dict> is not present, return a colon for each dimension.
+        If <explicit_dims> is True, include the variable's dimensions.
+        If <loop_subst> is True, apply a loop substitution, if found for any
+           missing dimension.
+        If <prepend_varname> is True, prepend the VarDDT's local name sequence.
+        """
+        if self.field is not None:
+            dimstr = self.field.call_dimstring(
+                var_dicts=var_dicts, explicit_dims=explicit_dims,
+                loop_subst=loop_subst, prepend_varname=prepend_varname)
+        # end if
+        # DH Copied from below: XXgoldyXX: Need to add dimensions to this
+        if prepend_varname:
+            dimstr = super().get_prop_value('local_name') + '%' + dimstr
+        return dimstr
+
     def call_string(self, var_dict, loop_vars=None):
         """Return a legal call string of this VarDDT's local name sequence.
         """
