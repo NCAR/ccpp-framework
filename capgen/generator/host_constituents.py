d2cb10b3b0009d5caba243b7ca6b7a4c0b6d9a13
Courtney Peverley <courtneyp@ucar.edu>
2026-07-09 20:07:08 -0600

    [feature/capgen-v1-cleanup-01] Make constituent index lookup routines case insensitive (#766)
    Adds to_lower routine to capgen/src/ccpp_constituent_prop_mod.F90 and uses that to make the ccpp_constituent_index, ccpp_constituent_indices, and <host>_const_get_index interfaces case insensitive.

--- capgen/generator/host_constituents.py
+++ capgen/generator/host_constituents.py
@@ -418,7 +418,7 @@ def _wrap_method_subs(host_dict) -> List[str]:
         'ccpp_const_get_index', 'const_index',
         [
             ('stdname', 'character(len=*), intent(in) :: stdname',
-             'standard_name=stdname'),
+             'standard_name=to_lower(stdname)'),
             ('const_index', 'integer, intent(out) :: const_index',
              'index=const_index'),
         ],
@@ -580,6 +580,7 @@ def _generate_host_constituents(
     lines.append('module {}'.format(_HOST_CONST_MOD))
     lines.append('')
     lines.append('{}use ccpp_kinds, only: kind_phys'.format(_INDENT))
+    lines.append('{}use ccpp_constituent_prop_mod, only: to_lower'.format(_INDENT))
     lines.append('{}use {}, only: &'.format(_INDENT, _CONST_PROP_MOD))
     lines.append('{}{}, &'.format(_INDENT * 2, _CONST_DDT))
     lines.append('{}{}, &'.format(_INDENT * 2, _CONST_PROP_TYPE))
